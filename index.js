51af1bf77aa02cf30370a0509f7a4c24ca13ff85
Mark Fowler <fowlermark488@gmail.com>
2017-10-12 01:49:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// code solution here
+class President {
+  constructor(name, politicalParty, yearsInOffice, homeState) {
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.yearsInOffice = yearsInOffice;
+    this.homeState = homeState;
+  }
+
+  veto() {
+    return "NO!";
+  }
+
+  passBill() {
+    return "You can do that!";
+  }
+
+  doCharity() {
+    return "I like to help people.";
+  }
+
+  conductPressInterview() {
+    return "I am proud to be an American.";
+  }
+
+  sayHi() {
+    return "Hi, my name is " + this.name + ". I am from " + this.homeState + ". I represent the " + this.politicalParty + "s, and was in office " + this.yearsInOffice + "."
+  }
+}
+
+new President("George Washington", "Federalist", "Virginia", 8);
+
+new President("Abraham Lincoln", "Republican", "Illinois", 4);
+
+new President("Richard Nixon", "Republican", "California", 5);
+
+new President("Jimmy Carter", "Democrat", "Georgia", 4);
